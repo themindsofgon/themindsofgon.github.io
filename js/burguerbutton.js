40986a51af2bc5a598f0ae994f0cc019ad8102a3
themindsofgon <themindsofgon@gmail.com>
2022-11-18 02:04:37 +0100

    v3

--- js/burguerbutton.js
+++ js/burguerbutton.js
@@ -1,12 +1,19 @@
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 
-document.querySelector(".bars__menu").addEventListener("click", animateBars);
+document.getElementById("back_menu").addEventListener("click", ocultar_menu);
 
-var line1__bars = document.querySelector(".line1__bars-menu");
-var line2__bars = document.querySelector(".line2__bars-menu");
-var line3__bars = document.querySelector(".line3__bars-menu");
+nav = document.getElementById("nav");
+background_menu = document.getElementById("back_menu");
 
-function animateBars(){
-    line1__bars.classList.toggle("activeline1__bars-menu");
-    line2__bars.classList.toggle("activeline2__bars-menu");
-    line3__bars.classList.toggle("activeline3__bars-menu");
+function mostrar_menu(){
+
+    nav.style.right = "0px";
+    background_menu.style.display = "block";
 }
+
+function ocultar_menu(){
+
+    nav.style.right = "-250px";
+    background_menu.style.display = "none";
+}
+
